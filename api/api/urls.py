bcce77e37d96c88d93483fd45bf5ca59a838f2f9
Alexandr Sarioglo <asarioglo@gmail.com>
2025-02-11 08:21:05 -0500

    Working login -> logout -> login -> logout... you get it
    Had to sacrifice the strip_url perk from Kong, because it also sent the
    stripped url to Keycloak for redirect on successful login. After login
    kong didn't know what to do with the stripped url. Bummer.

--- api/api/urls.py
+++ api/api/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('api/v1/', include('dev_api.urls')),
-    path('admin/', admin.site.urls),
+    path('dev_api/api/v1/', include('dev_api.urls')),
+    path('dev_api/admin/', admin.site.urls),
 ]
